a615295cfd047ab3d6d1bfb682eb936e10a55e95
Aref <aref.dehghanimo@gmail.com>
2025-08-02 00:38:44 -0700

    add routes

--- src/server.js
+++ src/server.js
@@ -1,14 +1,19 @@
 import express from 'express';
-
+// import autRoutes from './routes/auth';
+import taskRoutes from './routes/tasks.js';
 import { config } from './core/config/index.js';
 
 const app = express();
 const port = config.server.port;
 
-app.get('/',(req,res)=>{
-    res.send('hello world');
-});
+app.use(express.json());
 
+// app.use('/api',autRoutes);
+app.use('/api/tasks', taskRoutes);
+
+// app.get('/api/tasks',(req,res)=>{
+// res.send('helloworld')
+// })
 app.listen(port, ()=>{
     console.log(`Example app listening on port ${port}`)
 })
