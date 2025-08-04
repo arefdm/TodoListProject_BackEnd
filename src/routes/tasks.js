0adfa90e484c95e06d5d86d179acde746bf2d8d6
Aref <aref.dehghanimo@gmail.com>
2025-08-03 20:12:23 -0700

    improve path of delete route

--- src/routes/tasks.js
+++ src/routes/tasks.js
@@ -5,6 +5,6 @@ const router = express.Router();
 router.get('/:user_id',getTasks);
 router.post('/:user_id', createTask);
 router.put('/:user_id.:task_id', updateTask);
-router.delete('/:id', deleteTask);
+router.delete('/:task_id', deleteTask);
 
 export default router;
