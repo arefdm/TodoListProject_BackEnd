d1a3ab95bbd60b2ae5a566863c7eb9f22e24b288
Aref <aref.dehghanimo@gmail.com>
2025-08-13 13:38:49 -0700

    install cors to evoide cors error

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import authenticationRoutes from './routes/authentication.js';
 import taskRoutes from './routes/tasks.js';
 import { config } from './core/config/index.js';
@@ -6,6 +7,7 @@ import { config } from './core/config/index.js';
 const app = express();
 const port = config.server.port;
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', authenticationRoutes);
