a74ecdd179871dc3166459ba06f12852b85c0d63
Aref <aref.dehghanimo@gmail.com>
2025-08-18 19:49:27 -0700

    improve edit task

--- src/datamodel/taskData.js
+++ src/datamodel/taskData.js
@@ -1,6 +1,6 @@
 import { db } from "../core/database/connection.js";
 import { tasks } from "../core/database/schema.js";
-import { eq } from "drizzle-orm";
+import { eq,and } from "drizzle-orm";
 
 export const getUserTasks = async (userId) => {
     try {
@@ -27,8 +27,10 @@ export const editTask = async (taskId,userId,title,description,dueDate,status) =
     const currentDate = new Date();
     const newTask = await db.update(tasks)
     .set({ userId: userId , title: title, description: description, dueDate: dateOnly,status: status,updatedAt: currentDate })
-    .where(eq(tasks.id, taskId))
+    .where(and(eq(tasks.id, taskId), eq(tasks.userId, userId)))
     .returning({ updatedId: tasks.id });
+    console.log(newTask);
+    return newTask;
   } catch (err) {
     console.error(err.message);
   }
