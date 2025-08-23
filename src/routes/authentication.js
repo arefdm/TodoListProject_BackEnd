9971c6d31ad7da67304e5803cb2e9adfed451241
Aref <aref.dehghanimo@gmail.com>
2025-08-22 18:22:21 -0700

    add logout route

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { register,login, logedInUser } from '../controllers/authController.js';
+import { register,login, logedInUser,logout } from '../controllers/authController.js';
 import { authentication } from '../middlewares/authMiddelware.js';
 const router = express.Router();
 
 router.post('/register',register);
 router.post('/login',login);
+router.post('/logout',logout);
 router.use(authentication);
 router.get('/get_user',logedInUser);
 export default router;
