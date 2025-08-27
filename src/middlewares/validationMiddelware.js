0eaf1e4fe55867300547ccc7f408b5c2da63c09e
Aref <aref.dehghanimo@gmail.com>
2025-08-26 18:17:09 -0700

    improve error handling

--- src/middlewares/validationMiddelware.js
+++ src/middlewares/validationMiddelware.js
@@ -1,10 +1,14 @@
+import{ZodError} from 'zod';
+
 export const validation = (schema) => (req,res,next) =>{
     try {
         req.body = schema.parse(req.body);
         next();
     } catch (err) {
-        return res.status(400).json({
-            error: err.errors.map((e)=> e.message),
-        });
-    }
+        
+  
+            return res.status(400).json({
+              error: err.issues.map((e) => e.message).join(', '),
+            });
+        }   
 };
